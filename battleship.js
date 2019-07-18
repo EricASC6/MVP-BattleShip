2bc2090d0de521580d1a40cd50e1c156d820c015
EricASC6 <ericchen9025@gmail.com>
2019-07-18 10:17:18 -0400

    battleship

--- battleship.js
+++ battleship.js
@@ -17,17 +17,27 @@ let coly1 = 0;
 let colx2 = 60;
 let coly2 = 600;
 
-let randomx1 = Math.floor(Math.random()*10)*60
-let randomy1 = Math.floor(Math.random()*10)*60
-let randomx2 = Math.floor(Math.random()*10)*60
-let randomy2 = Math.floor(Math.random()*10)*60
-let randomx3 = Math.floor(Math.random()*10)*60
-let randomy3 = Math.floor(Math.random()*10)*60
-let randomx4 = Math.floor(Math.random()*10)*60
-let randomy4 = Math.floor(Math.random()*10)*60
-let randomx5 = Math.floor(Math.random()*10)*60
-let randomy5 = Math.floor(Math.random()*10)*60
+let randomx1;
+let randomy1;
+let randomx2;
+let randomy2;
+let randomx3;
+let randomy3;
+let randomx4;
+let randomy4;
+let randomx5;
+let randomy5;
 
+let xCoordinates = [randomx1, randomx2, randomx3, randomx4, randomx5]
+let yCoordinates = [randomy1, randomy2, randomy3, randomy4, randomy5]
+
+for (let i = 0; i < xCoordinates.length; i++){
+    xCoordinates[i] = Math.floor(Math.random()*10)*60;
+}
+
+for (let i = 0; i < yCoordinates.length; i++){
+    yCoordinates[i] = Math.floor(Math.random()*10)*60;
+}
 
 
 function setup(){
@@ -47,42 +57,53 @@ function draw(){
     }
 
     fill(shipColor1[0], shipColor1[1], shipColor1[2])
-    square(randomx1,randomy1,60)
+    square(xCoordinates[0], yCoordinates[0],60)
     fill(shipColor2[0], shipColor2[1], shipColor2[2])
-    square(randomx2,randomy2,60)
+    square(xCoordinates[1], yCoordinates[1],60)
     fill(shipColor3[0], shipColor3[1], shipColor3[2])
-    square(randomx3, randomy3, 60)
+    square(xCoordinates[2], yCoordinates[2],60)
     fill(shipColor4[0], shipColor4[1], shipColor4[2])
-    square(randomx4,randomy4,60)
+    square(xCoordinates[3], yCoordinates[3],60)
     fill(shipColor5[0], shipColor5[1], shipColor5[2])
-    square(randomx5,randomy5,60)
+    square(xCoordinates[4], yCoordinates[4],60)
 }
 
 
 
 function mouseClicked(){
-    if (dist(mouseX, mouseY, randomx1 + 30, randomy1 - 30) <= 30){
-        shipColor1[0] = 255;
-        shipColor1[1] = 0;
-        shipColor1[2] = 0;
-        fill("black")
-        text("Hit", randomx1  + 30, randomy1 - 30)
-    } else if (dist(mouseX, mouseY, randomx2 + 30, randomy2 - 30) <= Math.sqrt(900)){
-        shipColor2[0] = 255;
-        shipColor2[1] = 0;
-        shipColor2[2] = 0;
-    } else if (dist(mouseX, mouseY, randomx3 +30, randomy3 - 30) <= Math.sqrt(900)){
-        shipColor3[0] = 255;
-        shipColor3[1] = 0;
-        shipColor3[2] = 0;
-    } else if(dist(mouseX, mouseY, randomx4 + 30, randomy4 - 30) <= Math.sqrt(900)){
-        shipColor4[0] = 255;
-        shipColor4[1] = 0;
-        shipColor4[2] = 0;
-    } else if (dist(mouseX, mouseY, randomx5 +30 , randomy5 - 30) <= Math.sqrt(900)){
-        shipColor5[0] = 255;
-        shipColor5[1] = 0;
-        shipColor5[2] = 0;
+    squareX = Math.floor(mouseX/60) * 60
+    squareY = Math.floor(mouseY/60) * 60
+
+    for (let i = 0; i < xCoordinates.length; i++){
+        if (squareX === xCoordinates[i] && squareY === yCoordinates[i]){
+            shipColors[i][0] = 255;
+            shipColors[i][1] = 0;
+            shipColors[i][2] = 0;
+        }
     }
+
+
+
+    // if (dist(randomx1 + 30, randomy1 - 30, mouseX, mouseY) <= 30){
+    //     shipColor1[0] = 255;
+    //     shipColor1[1] = 0;
+    //     shipColor1[2] = 0;
+    // } else if (dist(randomx2 + 30, randomy2 - 30, mouseX, mouseY) <= 30){
+    //     shipColor2[0] = 255;
+    //     shipColor2[1] = 0;
+    //     shipColor2[2] = 0;
+    // } else if (dist(randomx3 +30, randomy3 - 30, mouseX, mouseY) <= 30){
+    //     shipColor3[0] = 255;
+    //     shipColor3[1] = 0;
+    //     shipColor3[2] = 0;
+    // } else if(dist(randomx4 + 30, randomy4 - 30, mouseX, mouseY) <= 30){
+    //     shipColor4[0] = 255;
+    //     shipColor4[1] = 0;
+    //     shipColor4[2] = 0;
+    // } else if (dist(randomx5 +30 , randomy5 - 30, mouseX, mouseY) <= 30){
+    //     shipColor5[0] = 255;
+    //     shipColor5[1] = 0;
+    //     shipColor5[2] = 0;
+    // }
 }
 
